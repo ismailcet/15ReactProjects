e6758162d00bf9700c4ad09235a9eb9e9ee6474e
ismailcet <ismailcet3@gmail.com>
2022-05-11 20:50:59 +0300

    Fourth Project

---
+++ 04-accordion/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import data from "./data";
+import Question from "./Question";
+
+function App() {
+  return (
+    <main>
+      <h1>Main Component</h1>
+      <Question></Question>
+    </main>
+  );
+}
+
+export default App;
---
+++ 04-accordion/src/components/Question.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = () => {
+  return <h1>Question component</h1>;
+};
+
+export default Question;
